a98d3d1b89e9b3c5df748f0c618f24bf4ae14f83
Breno Pereira <brenopereira856@gmail.com>
2025-05-20 14:31:34 -0300

    fix: remove files

--- src/config/auth0.ts
+++
@@ -1,8 +0,0 @@
-export const auth0Config = {
-  domain: import.meta.env.VITE_AUTH0_DOMAIN,
-  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  authorizationParams: {
-    redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
-    audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-  }
-}; 
